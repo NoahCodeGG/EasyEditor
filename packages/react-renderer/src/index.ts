e5e8125b0876f467cb2712278538575784db2dad
JinSooo <kimjinso@qq.com>
2024-12-12 22:21:42 +0800

    feat(react-renderer): change renderer name

--- packages/react-renderer/src/index.ts
+++ packages/react-renderer/src/index.ts
@@ -1,4 +1,4 @@
-export * from './schema-renderer'
-export * from './setting-renderer'
+export * from './component-renderer'
+export * from './configure-renderer'
 
 export const DEV = '_EASY_EDITOR_DEV_'
