665123039245c1fdcbb2b3821af01b99b873af6f
JinSo <kimjinso@qq.com>
2024-12-18 09:39:48 +0800

    feat(example-dashboard): add react-scan

--- examples/dashboard/src/main.tsx
+++ examples/dashboard/src/main.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import 'virtual:uno.css'
 import App from './App'
 import './index.css'
+
+if (typeof window !== 'undefined') {
+  scan({
+    enabled: true,
+    log: true, // logs render info to console (default: false)
+  })
+}
 
 createRoot(document.getElementById('root')!).render(<App />)
