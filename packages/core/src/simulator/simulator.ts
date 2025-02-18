313a9704f0e0d7eae298836c46aef4c0a5cefbb7
JinSo <kimjinso@qq.com>
2025-02-18 09:56:53 +0800

    fix(core): cancel default select rootNode

--- packages/core/src/simulator/simulator.ts
+++ packages/core/src/simulator/simulator.ts
@@ -288,7 +288,7 @@ export class Simulator {
         const { rootNode } = documentModel
         // const node = getClosestClickableNode(nodeInst?.node || rootNode, downEvent)
         // TODO: 默认不选择根节点，不然点击外部任意位置，都会选中根节点，后面想想有没有其他办法
-        const node = getClosestClickableNode(nodeInst?.node || rootNode, downEvent)
+        const node = getClosestClickableNode(nodeInst?.node, downEvent)
         // 如果找不到可点击的节点，直接返回
         if (!node) {
           return
