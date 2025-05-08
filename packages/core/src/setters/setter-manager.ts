bfb8388e6c854119309ab90c60ac50537b5cfbf4
JinSo <kimjinso@qq.com>
2025-05-08 21:24:34 +0800

    perf: materials & setters(2)

--- packages/core/src/setters/setter-manager.ts
+++ packages/core/src/setters/setter-manager.ts
@@ -1,9 +1,9 @@
 import { action } from 'mobx'
-import type { Node } from '..'
-import type { Component, SettingField } from '../designer'
+import type { Component, Node } from '..'
+import type { SettingField } from '../designer'
 import { createLogger } from '../utils'
 
-const logger = createLogger('SetterManager')
+const logger = createLogger('Setters')
 
 /**
  * for setter component props
@@ -28,40 +28,57 @@ interface RegisterSetterOption {
   overwrite?: boolean
 }
 
-export class SetterManager {
-  private _settersMap = new Map<
-    string,
-    Setter & {
-      type: string
-    }
-  >()
-
-  get settersMap() {
-    return this._settersMap
-  }
+export class Setters {
+  settersMap = new Map<string, Setter & { type: string }>()
 
   getSetter(type: string) {
-    return this._settersMap.get(type)
+    return this.settersMap.get(type)
   }
 
-  buildSettersMap = (setters: Record<string, Setter>) => {
-    Object.keys(setters).forEach(type => {
-      this.registerSetter(type, setters[type])
-    })
+  getSettersMap = () => {
+    return this.settersMap
   }
 
   @action
-  registerSetter(type: string, setter: Component | Setter, option?: RegisterSetterOption) {
-    if (this._settersMap.has(type) && !option?.overwrite) {
+  registerSetter(
+    typeOrMaps: string | { [key: string]: Component | Setter },
+    setter: Component | Setter,
+    option?: RegisterSetterOption,
+  ) {
+    if (typeof typeOrMaps === 'object') {
+      Object.keys(typeOrMaps).forEach(type => {
+        this.registerSetter(type, typeOrMaps[type])
+      })
+      return
+    }
+
+    if (!setter) {
+      return
+    }
+
+    if (this.settersMap.has(typeOrMaps) && !option?.overwrite) {
       logger.error(`SetterManager register error! The setter (${setter.name}) has already been registered!`)
       return
     }
 
-    const newSetter = {
-      component: setter,
-      title: (setter as Component).displayName || (setter as Setter).name || type,
+    // TODO: 暂时通过这样判断是 Setter 还是 Component
+    if (!('component' in setter)) {
+      setter = {
+        component: setter,
+        title: (setter as Component).displayName || (setter as Setter).name || typeOrMaps,
+      }
     }
-    this._settersMap.set(type, { type, ...newSetter })
+
+    if (!setter.initialValue) {
+      const initial = getInitialFromSetter(setter.component)
+      if (initial) {
+        setter.initialValue = (field: any) => {
+          return initial.call(field, field.getValue())
+        }
+      }
+    }
+
+    this.settersMap.set(typeOrMaps, { type: typeOrMaps, ...setter })
   }
 
   createSetterContent = (setter: any, props: Record<string, any>): Component => {
@@ -84,4 +101,11 @@ export class SetterManager {
       props,
     }
   }
+}
+
+const getInitialFromSetter = (setter: any) => {
+  return (
+    (setter && (setter.initial || setter.Initial || (setter.type && (setter.type.initial || setter.type.Initial)))) ||
+    null
+  )
 }
